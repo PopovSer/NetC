f4ef7d6a235f625b87360f22e532923d99a49cb6
PopovSer <seliarn@mail.ru>
2014-01-05 12:23:22 +0200

    -1

--- task42/task4/src/main/java/ua/netc/popov/task4/db/exception/DBException.java
+++
@@ -1,8 +0,0 @@
-package ua.netc.popov.task4.db.exception;
-
-public class DBException extends Exception{
-
-    public DBException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
